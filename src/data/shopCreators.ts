f19ee156234f6bf495eb885b085cfb761cc369bf
Gerriy <chui000831@gmail.com>
2026-06-28 22:48:28 +0800

    Connect ready Shopify collection filters

--- src/data/shopCreators.ts
+++ src/data/shopCreators.ts
@@ -24,12 +24,25 @@ export const creatorFilters: ShopCreatorFilter[] = [
     label: 'Ahua',
     collectionId: '319252594735',
   },
-  { slug: 'chowee', label: 'Chowee' },
-  { slug: 'gerri', label: 'Gerri' },
-  { slug: 'p-chan', label: 'P-Chan' },
+  {
+    slug: 'chowee',
+    label: 'Chowee',
+    collectionId: '319252693039',
+  },
+  {
+    slug: 'gerri',
+    label: 'Gerri',
+    collectionId: '319252758575',
+  },
+  {
+    slug: 'p-chan',
+    label: 'P-Chan',
+    collectionId: '319252824111',
+  },
   {
     slug: 'yy',
     label: 'YY',
     collectionId: '319252889647',
   },
+  { slug: 'riki', label: 'Riki' },
 ];
